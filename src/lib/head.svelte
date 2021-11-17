c8ecb14cb1379d497a63a90645eacd681caa3906
Masaya Shida <masayashida@gmail.com>
2021-11-17 17:21:03 +0700

    initial commit

---
+++ src/lib/head.svelte
@@ -0,0 +1,40 @@
+
+<svelte:head>
+    <!-- head content -->
+      <link rel="stylesheet" href="/styles/reset.css">
+      <link rel="stylesheet" href="/styles/globals.css">
+  
+      <!-- favicons -->
+      <link rel="apple-touch-icon" sizes="57x57" href="/favicons/apple-icon-57x57.png">
+      <link rel="apple-touch-icon" sizes="60x60" href="/favicons/apple-icon-60x60.png">
+      <link rel="apple-touch-icon" sizes="72x72" href="/favicons/apple-icon-72x72.png">
+      <link rel="apple-touch-icon" sizes="76x76" href="/favicons/apple-icon-76x76.png">
+      <link rel="apple-touch-icon" sizes="114x114" href="/favicons/apple-icon-114x114.png">
+      <link rel="apple-touch-icon" sizes="120x120" href="/favicons/apple-icon-120x120.png">
+      <link rel="apple-touch-icon" sizes="144x144" href="/favicons/apple-icon-144x144.png">
+      <link rel="apple-touch-icon" sizes="152x152" href="/favicons/apple-icon-152x152.png">
+      <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-icon-180x180.png">
+      <link rel="icon" type="image/png" sizes="192x192"  href="/favicons/android-icon-192x192.png">
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png">
+      <link rel="icon" type="image/png" sizes="96x96" href="/favicons/favicon-96x96.png">
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png">
+      <link rel="manifest" href="/favicons/manifest.json">
+      <meta name="msapplication-TileColor" content="#ffffff">
+      <meta name="msapplication-TileImage" content="/favicons/ms-icon-144x144.png">
+      <meta name="theme-color" content="#ffffff">
+  
+      <meta name="keywords" content="masayashida, masaya shida, shida masaya, masaya shida portfolio, works by masaya shida">
+  
+      <!-- Open Graph / Facebook -->
+      <meta property="og:type" content="website">
+      <meta property="og:title" content="Masaya Shida">
+      <meta property="og:description" content="Personal portfolio homepage for Cambodia-based web developer.">
+      <meta property="og:image" content="/static/images/banner-logo-margins.png">
+  
+      <!-- Twitter -->
+      <meta property="twitter:card" content="summary_large_image">
+      <meta property="twitter:title" content="Masaya Shida">
+      <meta property="twitter:description" content="Personal portfolio homepage for Cambodia-based web developer.">
+      <meta property="twitter:image" content="/static/images/banner-logo-margins.png">
+  
+  </svelte:head>
---
+++ src/routes/_layout.svelte
@@ -0,0 +1,24 @@
+<script>
+  import Nav from '$lib/nav.svelte'
+</script>
+
+<Nav></Nav>
+<main>
+    <slot></slot>
+</main>
+
+<style>
+  /* @import '/static/styles/globals.css'; */
+  main {
+    padding: 0 10%;
+    margin: 5% auto;
+    max-width: 1280px;
+    /* border: 1px solid red; */
+  }
+  @media(max-width: 1023px) {
+    main {padding: 10% 10%}
+  }
+  @media(max-width: 767px) {
+    main {padding: 7.5% 7.5%}
+  }
+</style>
