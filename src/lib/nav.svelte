2ec4d805d25ceb91fa0eb2f66175fddd98c201fb
Masaya Shida <masayashida@gmail.com>
2021-11-21 17:31:22 +0700

    initial commit

---
+++ src/lib/nav.svelte
@@ -0,0 +1,28 @@
+<div class="navContainer">
+    <img src="/images/SVG/masaya-web-dev-logo.svg" alt="Logo">
+    <nav>
+        <ul>
+            <li>
+                <a href="/">Projects</a>
+            </li>
+            <li>
+                <a href="/">Contact</a>
+            </li>
+            <li>
+                <a href="/" lang="khm">ភាសាខ្មែរ</a>
+            </li>
+        </ul>
+    </nav>
+</div>
+
+<style>
+    @import '../../static/styles/nav.module.css';
+    ul {
+        display: flex;
+        list-style-type: none;
+        margin: 0;
+    }
+    li {
+        line-height: 1;
+    }
+</style>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Nav from '$lib/nav.svelte'
+</script>
+
+<Nav></Nav>
+<main>
+    <slot />
+</main>
+
+<style>
+  main {
+    /* padding: 10% 0; */
+    margin: 0 auto;
+  }
+</style>
