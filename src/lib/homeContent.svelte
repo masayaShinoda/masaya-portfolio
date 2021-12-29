82a72577b4f080263d617754b3bb61b2b942c3a8
Masaya Shida <masayashida@gmail.com>
2021-12-29 17:04:45 +0700

    updated homepage

--- src/lib/homeContent.svelte
+++ src/lib/homeContent.svelte
@@ -3,9 +3,8 @@
 
     const contentSection1 =
 `
-## Design Philosophy - Minimal & Practical
-I believe that in this busy world, we tend to overlook how important it is to keep things simple and clutterless. My design approach is to deliver essential information in an easy-to-read manner, and make webpages fast with navigation that is as straightforward as possible.
-
+## Design Philosophy: Minimal & Practical 
+I believe that in this busy world, we tend to overlook how important it is to keep things simple and clutterless. My design approach is to deliver essential information in an easy-to-read manner, and speedy websites with straightforward navigation.
 `
 </script>
 
@@ -14,5 +13,6 @@ I believe that in this busy world, we tend to overlook how important it is to ke
         <span>
             <SvelteMarkdown source={contentSection1} />
         </span>
+        <img src="/images/SVG/golden-ratio.svg" alt="golden ratio spiral">
     </div>
 </section>
