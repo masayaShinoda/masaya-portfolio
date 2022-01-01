a424bfd91b3e5f7a1724ea6027dd3fc6093c5510
Masaya Shida <masayashida@gmail.com>
2022-01-01 16:01:51 +0700

    homeContent style update

--- src/lib/homeContent.svelte
+++ src/lib/homeContent.svelte
@@ -9,18 +9,23 @@
 ## Design Philosophy: Minimal & Practical 
 I believe that in this busy world, we tend to overlook how important it is to keep things simple and clutterless. My design approach is to deliver essential information in an easy-to-read manner, and speedy websites with straightforward navigation.
 `,
-//     image:  
-// {
-//     src: "/images/SVG/golden-ratio.svg",
-//     alt: "illustration of golden ratio spiral"
-// }
+    image:  
+{
+    src: "/images/SVG/golden-ratio.svg",
+    alt: "illustration of golden ratio spiral"
+}
 },
 {
     content:
 `
 ## Accessibility as a necessity
 Since the web is a platform that is accessed by a wide variety of devices, and by people with physical or cognitive challenges, it matters to me that webpages pass essential [WCAG](https://www.w3.org/WAI/WCAG21/quickref/) standards such as perceivability, distinguishability, and operability in order to try to be as accessible as possible.
-`
+`,
+    image:
+{
+    src: "/images/SVG/accessibility-icon.svg",
+    alt: "illustration of touchscreen interaction"
+}
 }
 ]
 
@@ -34,7 +39,7 @@ Since the web is a platform that is accessed by a wide variety of devices, and b
             <span>
                 <SvelteMarkdown source={section[`content`]} />
                 {#if section['image']}
-                <img src={section['image']['src']} alt={section['image']['alt']} />
+                    <img src={section['image']['src']} alt={section['image']['alt']} />
                 {/if}
             </span>
         {/each}
