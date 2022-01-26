b5f60559761b4679d6fcc16b66c20e68335401ae
Masaya Shida <masayashida@gmail.com>
2022-01-26 21:20:05 +0700

    added new portf item

--- src/lib/projects.svelte
+++ src/lib/projects.svelte
@@ -4,22 +4,27 @@
 `
 - 
 [
-![screenshot of blog.masayashida.com](/images/1280w/masayas-thoughts_result.png)
+![screenshot of Zillion United's corporate website](/images/1280w/z1-corporate_result.webp)
+Trading App Corporate Website
+](https://zillion-united-frontend.vercel.app)
+- 
+[
+![screenshot of blog.masayashida.com](/images/1280w/masayas-thoughts_result.webp)
 Personal blog website
 ](https://blog.masayashida.com)
 -
 [
-![screenshot of cryptocalc-site.vercel.app](/images/1280w/cryptocalc_result.png)
+![screenshot of cryptocalc-site.vercel.app](/images/1280w/cryptocalc_result.webp)
 Cryptocurrency investment calculator
 ](https://cryptocalc-site.vercel.app/)
 -
 [
-![screenshot of sentinelspropertyalliance.com](/images/1280w/sentinelsproperty_result.png)
+![screenshot of sentinelspropertyalliance.com](/images/1280w/sentinelsproperty_result.webp)
 Real estate listings website
 ](https://sentinelspropertyalliance.com)
 -
 [
-![screenshot of FreeCodeCamp certificate](/images/1x/fcc-certif.png)
+![screenshot of Masaya's FreeCodeCamp certificate](/images/1x/fcc-certif.png)
 FCC Certification projects
 ](https://fcc.masayashida.com)
 `
