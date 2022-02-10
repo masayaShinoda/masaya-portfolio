330d677d46125dbd74f9bf74af2e4d18f53d9f43
Masaya Shida <masayashida@gmail.com>
2022-02-10 20:51:20 +0700

    changed images webp to png

--- src/lib/projects.svelte
+++ src/lib/projects.svelte
@@ -4,27 +4,27 @@
 `
 - 
 [
-![screenshot of Good Time Hospitality Group website](/images/1280w/gt-rr-mockup-laptop-phone_result.webp)
+![screenshot of Good Time Hospitality Group website](/images/1280w/gt-rr-mockup-laptop-phone_result.png)
 Hospitality & Resort Website
 ](https://goodtimehospitality.com/)
 - 
 [
-![screenshot of Zillion United's corporate website](/images/1280w/z1-corporate_result.webp)
+![screenshot of Zillion United's corporate website](/images/1280w/z1-corporate_result.png)
 Trading App Corporate Website
 ](https://zillionunited.io)
 - 
 [
-![screenshot of blog.masayashida.com](/images/1280w/masayas-thoughts_result.webp)
+![screenshot of blog.masayashida.com](/images/1280w/masayas-thoughts_result.png)
 Personal blog website
 ](https://blog.masayashida.com)
 -
 [
-![screenshot of cryptocalc-site.vercel.app](/images/1280w/cryptocalc_result.webp)
+![screenshot of cryptocalc-site.vercel.app](/images/1280w/cryptocalc_result.png)
 Cryptocurrency investment calculator
 ](https://cryptocalc-site.vercel.app/)
 -
 [
-![screenshot of sentinelspropertyalliance.com](/images/1280w/sentinelsproperty_result.webp)
+![screenshot of sentinelspropertyalliance.com](/images/1280w/sentinelsproperty_result.png)
 Real estate listings website
 ](https://sentinelspropertyalliance.com)
 -
