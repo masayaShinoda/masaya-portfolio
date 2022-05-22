1800a889ae1b2289376aa522915256c1f26a429a
Masaya Shida <masayashida@gmail.com>
2022-05-22 16:32:40 +0700

    update portfolio add HLH

--- src/lib/projects.svelte
+++ src/lib/projects.svelte
@@ -4,33 +4,38 @@
 `
 -
 [
+![screenshot of Hong Ly Heng Auto Parts website](/images/1280w/hlh-mockup-laptop-phone_result.png)
+Car Parts Shop Website
+](https://hlhparts.com/)
+-
+[
 ![screenshot of Good Time Hospitality Group website](/images/1280w/gt-rr-mockup-laptop-phone_result.png)
 Hospitality & Resort Website
 ](https://goodtimehospitality.com/)
 -
 [
 ![screenshot of Zillion United's corporate website](/images/1280w/z1-corporate_result.png)
-Trading App Corporate Website
+Finance Sector Corporate Website
 ](https://zillionunited.io)
 -
 [
 ![screenshot of blog.masayashida.com](/images/1280w/masayas-thoughts_result.png)
-Personal blog website
+Personal Blog Website
 ](https://blog.masayashida.com)
 -
 [
 ![screenshot of cryptocalc-site.vercel.app](/images/1280w/cryptocalc_result.png)
-Cryptocurrency investment calculator
+Crypto Investment Calculator
 ](https://cryptocalc-site.vercel.app/)
 -
 [
 ![screenshot of sentinelspropertyalliance.com](/images/1280w/sentinelsproperty_result.png)
-Real estate listings website
+Local Property Listings Website
 ](https://sentinelspropertyalliance.com)
 -
 [
 ![screenshot of Masaya's FreeCodeCamp certificate](/images/1x/fcc-certif.png)
-FCC Certification projects
+FCC Certification Projects
 ](https://fcc.masayashida.com)
 `
 
